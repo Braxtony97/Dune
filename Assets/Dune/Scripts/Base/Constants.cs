e797e2b502672901e3c4d6e3de412f80340c9a7c
Braxtony97 <renat_samigullin_97@mail.ru>
2024-08-04 00:19:43 +0300

    create base scripts to control the project

---
+++ Assets/Dune/Scripts/Base/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Dune.Scripts.Base
+{
+    public class Constants
+    {
+        public const float SCREEN_APPEARING_TIME = 0.25f;
+        public const float SCREEN_DISAPPERARING_TIME = 0.15f;
+    }
+}
